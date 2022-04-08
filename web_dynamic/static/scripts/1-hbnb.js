37a73bdebdc18f745ca330bda4cea418fafdc0b6
adamclear <3589@holbertonschool.com>
2022-04-08 22:07:46 +0000

    updated file 1-hbnb files

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,20 @@
-const amenityList = []
+const amenityList = [];
 $(document).ready(function() {
-  $('input[type=checkbox]').change(
-    function() {
-        if (this.checked) {
-          amenityList.push(this.data('name'));
-        } else if (this.unchecked) {
-          amenityList.remove(this.data('name'));
+  $('input[type=checkbox]').change(function() {
+        if ($(this).is(':checked')) {
+          amenityList.push($(this).data('name'));
+        } else {
+          amenityList.splice(amenityList.indexOf($(this).data('name')), 1);
         }
-
         if (amenityList.length > 0) {
+          $('.amenities h4').css("width", "250px");
+          $('.amenities h4').css("height", "25px");
+          $('.amenities h4').css("overflow", "hidden");
+          $('.amenities h4').css("white-space", "nowrap");
+          $('.amenities h4').css("text-overflow", "ellipsis");
           $('.amenities h4').text(amenityList.sort().join(', '));
+        } else {
+          $('.amenities h4').text(String.fromCharCode(160));
         }
     });
 });
