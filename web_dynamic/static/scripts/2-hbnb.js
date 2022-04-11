e0099b04f82153e03d874a6ad4bc5405681753d0
Kaylee Dao <kayleed2@gmail.com>
2022-04-11 14:14:18 -0500

    Red light, gray light

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 });
 
 
-$.get('http://localhost:5001/api/v1/status', (data) => {
+$.get('http://0.0.0.0:5001/api/v1/status', (data) => {
   if (data.status == 'OK') {
     $('div#api_status').addClass('available');
   } else {
